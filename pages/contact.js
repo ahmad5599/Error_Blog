5065af09b746b752abf0544091a0878ce5b98373
Ahmad Hamid <ahmad.hamid599@outlook.com>
2022-08-05 00:56:24 +0500

    2nd initial commit

--- pages/contact.js
+++ pages/contact.js
@@ -1,21 +1,21 @@
-import Head from "next/head";
 import React from "react";
 import styles from "../styles/Home.module.css";
+import Head from "next/head";
 import Navbar from "./navbar";
 
-function about() {
+function ContactMe() {
   return (
     <div className={styles.container}>
       <Navbar />
       <Head>
-        <title>About</title>
+        <title>Contact</title>
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>About</h1>
+        <h1 className={styles.title}>Contact Me</h1>
       </main>
     </div>
   );
 }
 
-export default about;
+export default ContactMe;
---
+++ pages/navbar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+
+function navbar() {
+  return (
+    <div>
+      <nav className={styles.nav}>
+        <ul>
+          {/* <li>
+        <span className={styles.logo}>
+          <Image
+            src="/logo.png"
+            alt="Error BlogLogo"
+            width={32}
+            height={19}
+          ></Image>
+        </span>
+      </li> */}
+          <Link href="/posts">
+            <a>
+              <li>Posts</li>
+            </a>
+          </Link>
+          <Link href="https://github.com">
+            <a target="_blank">
+              <li>Git</li>
+            </a>
+          </Link>
+          <Link href="/contact">
+            <a>
+              <li>Contact</li>
+            </a>
+          </Link>
+          <Link href="/about">
+            <a>
+              <li>About</li>
+            </a>
+          </Link>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default navbar;
